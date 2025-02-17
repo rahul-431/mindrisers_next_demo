1ffc191d667dc518757c3c7080b9f501137551c1
Rahul Mijar <rahulmijar0243@gmail.com>
2025-02-17 13:34:50 +0545

    feat: add reponsive footer

--- components/shared/IconLink.tsx
+++ components/shared/IconLink.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 interface IconLinkType {
   icon: React.ReactNode;
   href: string;
+  iconSize?: "sm" | "lg" | "xl" | "2xl";
 }
-const IconLink = ({ icon, href }: IconLinkType) => {
+const IconLink = ({ icon, href, iconSize }: IconLinkType) => {
   return (
-    <Link href={href} className="hover:text-[#BA933E]">
+    <Link
+      href={href}
+      className={`hover:text-[#BA933E] text-${iconSize ? iconSize : "lg"}`}
+    >
       {icon}
     </Link>
   );
